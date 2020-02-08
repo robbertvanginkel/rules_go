0e7d13dd286cf5d62ae4e784432a543b2eeec3d4
Robbert Van Ginkel <robbert@uber.com>
2020-02-07 18:16:54 -0800

    Respect -test.v for stdout printing

--- go/tools/testwrapper/wrap.go
+++ go/tools/testwrapper/wrap.go
@@ -15,7 +15,7 @@
 package main
 
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -23,6 +23,8 @@ import (
 	"os"
 	"os/exec"
 	"strconv"
+	"strings"
+	"time"
 )
 
 // testWrapperAbnormalExit is used by the testwrapper to indicate the child
@@ -42,19 +44,170 @@ func shouldWrap() bool {
 	return ok
 }
 
-func wrap(pkg string) error {
-	var jsonBuffer bytes.Buffer
-	jsonConverter := NewConverter(&jsonBuffer, pkg, Timestamp)
+func isVerbose(args []string) bool {
+	for _, s := range args {
+		if s == "-test.v" {
+			return true
+		}
+	}
+	return false
+}
 
+// jsonEvent as encoded by the test2json package.
+type jsonEvent struct {
+	Time    *time.Time
+	Action  string
+	Package string
+	Test    string
+	Elapsed *float64
+	Output  string
+}
+
+type testCase struct {
+	state    string
+	output   strings.Builder
+	duration *float64
+}
+
+type testSuite struct {
+	byName   map[string]*testCase
+	duration *float64
+	name     string
+	events   []*jsonEvent
+}
+
+func (s *testSuite) named(name string) *testCase {
+	if name == "" {
+		return nil
+	}
+	if _, ok := s.byName[name]; !ok {
+		s.byName[name] = &testCase{}
+	}
+	return s.byName[name]
+}
+
+func (s *testSuite) failed(name string) bool {
+	if t, ok := s.byName[name]; ok {
+		return t.state == "fail"
+	}
+	return false
+}
+
+type ingester struct {
+	closers   []io.Closer
+	converter io.WriteCloser
+	tests     testSuite
+	out       io.Writer
+	verbose   bool
+}
+
+func newIngester(pkg string, verbose bool, out io.Writer) *ingester {
+	r, w := io.Pipe()
+	i := &ingester{
+		closers: []io.Closer{w},
+		tests: testSuite{
+			byName: make(map[string]*testCase),
+			name:   pkg,
+		},
+		out:     out,
+		verbose: verbose,
+	}
+	go i.decodeJSONEvents(r)
+	i.converter = NewConverter(w, pkg, Timestamp)
+	return i
+}
+
+func (i *ingester) decodeJSONEvents(r *io.PipeReader) {
+	dec := json.NewDecoder(r)
+	for {
+		var e jsonEvent
+		if err := dec.Decode(&e); err == io.EOF {
+			break
+		} else if err != nil {
+			r.CloseWithError(err)
+			break
+		}
+		i.handleJSONEvent(&e)
+	}
+}
+
+func (i *ingester) Write(p []byte) (int, error) {
+	if i.verbose {
+		i.out.Write(p)
+	}
+	return i.converter.Write(p)
+}
+
+func (i *ingester) Close() error {
+	for _, c := range i.closers {
+		c.Close()
+	}
+	if !i.verbose {
+		for _, e := range i.tests.events {
+			if e.Test == "" || i.tests.failed(e.Test) {
+				i.out.Write([]byte(e.Output))
+			}
+		}
+	}
+	return i.converter.Close()
+}
+
+func (i *ingester) handleJSONEvent(e *jsonEvent) {
+	i.tests.events = append(i.tests.events, e)
+	switch s := e.Action; s {
+	case "run":
+		if c := i.tests.named(e.Test); c != nil {
+			c.state = s
+		}
+	case "output":
+		if c := i.tests.named(e.Test); c != nil {
+			c.output.WriteString(e.Output)
+		}
+	case "skip":
+		if c := i.tests.named(e.Test); c != nil {
+			c.output.WriteString(e.Output)
+			c.state = s
+			c.duration = e.Elapsed
+		}
+	case "fail":
+		if c := i.tests.named(e.Test); c != nil {
+			c.state = s
+			c.duration = e.Elapsed
+		} else {
+			i.tests.duration = e.Elapsed
+		}
+	case "pass":
+		if c := i.tests.named(e.Test); c != nil {
+			c.duration = e.Elapsed
+			c.state = s
+		} else {
+			i.tests.duration = e.Elapsed
+		}
+	}
+}
+
+func (i *ingester) writeXML(path string) error {
+	xml, err := toXML(i.tests.name, i.tests.duration, i.tests.byName)
+	if err != nil {
+		return fmt.Errorf("error converting test output to xml: %s", err)
+	}
+	if err := ioutil.WriteFile(path, xml, 0664); err != nil {
+		return fmt.Errorf("error writing test xml: %s", err)
+	}
+	return nil
+}
+
+func wrap(pkg string) error {
+	ingester := newIngester(pkg, isVerbose(os.Args[1:]), os.Stdout)
 	args := append([]string{"-test.v"}, os.Args[1:]...)
 	cmd := exec.Command(os.Args[0], args...)
 	cmd.Env = append(os.Environ(), "GO_TEST_WRAP=0")
 	cmd.Stderr = os.Stderr
-	cmd.Stdout = io.MultiWriter(os.Stdout, jsonConverter)
+	cmd.Stdout = ingester
 	err := cmd.Run()
-	jsonConverter.Close()
+	ingester.Close()
 	if out, ok := os.LookupEnv("XML_OUTPUT_FILE"); ok {
-		werr := writeReport(jsonBuffer, pkg, out)
+		werr := ingester.writeXML(out)
 		if werr != nil {
 			if err != nil {
 				return fmt.Errorf("error while generating testreport: %s, (error wrapping test execution: %s)", werr, err)
@@ -63,15 +216,4 @@ func wrap(pkg string) error {
 		}
 	}
 	return err
-}
-
-func writeReport(jsonBuffer bytes.Buffer, pkg string, path string) error {
-	xml, cerr := json2xml(&jsonBuffer, pkg)
-	if cerr != nil {
-		return fmt.Errorf("error converting test output to xml: %s", cerr)
-	}
-	if err := ioutil.WriteFile(path, xml, 0664); err != nil {
-		return fmt.Errorf("error writing test xml: %s", err)
-	}
-	return nil
 }
--- go/tools/testwrapper/xml.go
+++ go/tools/testwrapper/xml.go
@@ -15,14 +15,10 @@
 package main
 
 import (
-	"encoding/json"
 	"encoding/xml"
 	"fmt"
-	"io"
 	"path"
 	"sort"
-	"strings"
-	"time"
 )
 
 type xmlTestSuites struct {
@@ -57,81 +53,7 @@ type xmlMessage struct {
 	Contents string `xml:",chardata"`
 }
 
-// jsonEvent as encoded by the test2json package.
-type jsonEvent struct {
-	Time    *time.Time
-	Action  string
-	Package string
-	Test    string
-	Elapsed *float64
-	Output  string
-}
-
-type testCase struct {
-	state    string
-	output   strings.Builder
-	duration *float64
-}
-
-// json2xml converts test2json's output into an xml output readable by Bazel.
-// http://windyroad.com.au/dl/Open%20Source/JUnit.xsd
-func json2xml(r io.Reader, pkgName string) ([]byte, error) {
-	var pkgDuration *float64
-	testcases := make(map[string]*testCase)
-	testCaseByName := func(name string) *testCase {
-		if name == "" {
-			return nil
-		}
-		if _, ok := testcases[name]; !ok {
-			testcases[name] = &testCase{}
-		}
-		return testcases[name]
-	}
-
-	dec := json.NewDecoder(r)
-	for {
-		var e jsonEvent
-		if err := dec.Decode(&e); err == io.EOF {
-			break
-		} else if err != nil {
-			return nil, fmt.Errorf("error decoding test2json output: %s", err)
-		}
-		switch s := e.Action; s {
-		case "run":
-			if c := testCaseByName(e.Test); c != nil {
-				c.state = s
-			}
-		case "output":
-			if c := testCaseByName(e.Test); c != nil {
-				c.output.WriteString(e.Output)
-			}
-		case "skip":
-			if c := testCaseByName(e.Test); c != nil {
-				c.output.WriteString(e.Output)
-				c.state = s
-				c.duration = e.Elapsed
-			}
-		case "fail":
-			if c := testCaseByName(e.Test); c != nil {
-				c.state = s
-				c.duration = e.Elapsed
-			} else {
-				pkgDuration = e.Elapsed
-			}
-		case "pass":
-			if c := testCaseByName(e.Test); c != nil {
-				c.duration = e.Elapsed
-				c.state = s
-			} else {
-				pkgDuration = e.Elapsed
-			}
-		}
-	}
-
-	return xml.MarshalIndent(toXML(pkgName, pkgDuration, testcases), "", "\t")
-}
-
-func toXML(pkgName string, pkgDuration *float64, testcases map[string]*testCase) *xmlTestSuites {
+func toXML(pkgName string, pkgDuration *float64, testcases map[string]*testCase) ([]byte, error) {
 	cases := make([]string, 0, len(testcases))
 	for k := range testcases {
 		cases = append(cases, k)
@@ -177,5 +99,5 @@ func toXML(pkgName string, pkgDuration *float64, testcases map[string]*testCase)
 		}
 		suite.TestCases = append(suite.TestCases, newCase)
 	}
-	return &xmlTestSuites{Suites: []xmlTestSuite{suite}}
+	return xml.MarshalIndent(&xmlTestSuites{Suites: []xmlTestSuite{suite}}, "", "\t")
 }
